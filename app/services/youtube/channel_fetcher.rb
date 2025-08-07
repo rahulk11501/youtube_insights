35b613d1912c93d4013232394cf7590abb0a804b
rahulk11501 <rahulkumar11501@gmail.com>
2025-08-07 06:28:42 +0000

    Removinf youtube_fetcher

--- app/services/youtube/channel_fetcher.rb
+++ app/services/youtube/channel_fetcher.rb
@@ -1,19 +1,115 @@
 # frozen_string_literal: true
 
 module Youtube
-  class ChannelFetcher
-    def search_channels(query)
-      # Replace this with actual API integration
-      # For now, return a stubbed response or delegated logic
-      YoutubeFetcher.new.search_channels(query)
+    class ChannelFetcher
+        include HTTParty
+        base_uri 'https://www.googleapis.com/youtube/v3'
+
+        def initialize(api_key = ENV['YOUTUBE_API_KEY'])
+            @api_key = api_key
+        end
+
+        def fetch_channel(channel_id: nil, channel_name: nil, handle_or_url: nil)
+            resolved_id = channel_id || resolve_channel_id(channel_name, handle_or_url)
+            raise ArgumentError, "Unable to resolve a valid YouTube channel ID" unless resolved_id
+
+            query_params = {
+                part: 'snippet,statistics',
+                id: resolved_id,
+                key: @api_key
+            }
+
+            response = self.class.get('/channels', query: query_params)
+            log_response(response)
+
+            return nil unless response.success? && response.parsed_response['items'].present?
+
+            item = response.parsed_response['items'].first
+            format_channel_response(item)
+        end
+
+        def search_channels(query, max_results = 5)
+            response = self.class.get('/search', query: {
+                part: 'snippet',
+                q: query,
+                type: 'channel',
+                maxResults: max_results,
+                key: @api_key
+            })
+
+            log_response(response)
+            return [] unless response.success?
+
+            response.parsed_response['items'].map do |item|
+            {
+                channel_id: item.dig('snippet', 'channelId'),
+                title: item.dig('snippet', 'title'),
+                description: item.dig('snippet', 'description'),
+                thumbnail_url: item.dig('snippet', 'thumbnails', 'default', 'url')
+            }
+            end
+        end
+
+        private
+
+        def resolve_channel_id(channel_name, handle_or_url)
+            return nil unless channel_name || handle_or_url
+
+            query = channel_name || extract_handle_from_url(handle_or_url)
+            return nil if query.blank?
+
+            response = self.class.get('/search', query: {
+                part: 'snippet',
+                q: query,
+                type: 'channel',
+                maxResults: 1,
+                key: @api_key
+            })
+
+            log_response(response)
+            response.parsed_response.dig('items', 0, 'snippet', 'channelId')
+        rescue => e
+            Rails.logger.error("[ChannelFetcher] resolve_channel_id error: #{e.message}")
+            nil
+        end
+
+        def extract_handle_from_url(url)
+            return nil if url.blank?
+
+            uri = URI.parse(url)
+            path = uri.path
+
+            if path =~ %r{/@([\w\-]+)}
+            Regexp.last_match(1)
+            elsif path =~ %r{/c/([\w\-]+)}
+            Regexp.last_match(1)
+            elsif path =~ %r{/user/([\w\-]+)}
+            Regexp.last_match(1)
+            else
+            nil
+            end
+        rescue URI::InvalidURIError
+            nil
+        end
+
+        def format_channel_response(item)
+            {
+                channel_id: item['id'],
+                title: item.dig('snippet', 'title'),
+                description: item.dig('snippet', 'description'),
+                thumbnail_url: item.dig('snippet', 'thumbnails', 'default', 'url'),
+                subscriber_count: item.dig('statistics', 'subscriberCount')&.to_i,
+                video_count: item.dig('statistics', 'videoCount')&.to_i,
+                view_count: item.dig('statistics', 'viewCount')&.to_i,
+                last_fetched_at: Time.current
+            }
+        end
+
+        def log_response(response)
+            return unless Rails.env.development?
+
+            puts "[YouTube API] #{response.code} - #{response.parsed_response.inspect}"
+        end
     end
 
-    def fetch_channel(channel_id: nil, channel_name: nil, handle_or_url: nil)
-      YoutubeFetcher.new.fetch_channel(
-        channel_id: channel_id,
-        channel_name: channel_name,
-        handle_or_url: handle_or_url
-      )
-    end
-  end
 end
--- app/services/youtube/video_fetcher.rb
+++ app/services/youtube/video_fetcher.rb
@@ -1,13 +1,71 @@
 # frozen_string_literal: true
 
 module Youtube
-  class VideoFetcher
-    def search_videos(query)
-      YoutubeFetcher.new.search_videos(query)
+    class VideoFetcher
+        include HTTParty
+        base_uri 'https://www.googleapis.com/youtube/v3'
+
+    def initialize(api_key = ENV['YOUTUBE_API_KEY'])
+        @api_key = api_key
     end
 
     def fetch_video(video_id)
-      YoutubeFetcher.new.fetch_video(video_id)
+        response = self.class.get('/videos', query: {
+        part: 'snippet,statistics',
+        id: video_id,
+        key: @api_key
+        })
+
+        log_response(response)
+        return nil unless response.success?
+
+        item = response.parsed_response['items']&.first
+        return nil unless item
+
+        {
+            video_id: item['id'],
+            title: item['snippet']['title'],
+            description: item['snippet']['description'],
+            thumbnail_url: item['snippet']['thumbnails']['high']['url'],
+            published_at: item['snippet']['publishedAt'],
+            channel_id: item['snippet']['channelId'],
+            channel_title: item['snippet']['channelTitle'],
+            view_count: item.dig('statistics', 'viewCount')&.to_i,
+            like_count: item.dig('statistics', 'likeCount')&.to_i,
+            comment_count: item.dig('statistics', 'commentCount')&.to_i,
+            last_fetched_at: Time.current
+        }
     end
-  end
+
+    def search_videos(query)
+        response = self.class.get('/search', query: {
+            part: 'snippet',
+            q: query,
+            type: 'video',
+            maxResults: 10,
+            key: @api_key
+        })
+
+        log_response(response)
+        return [] unless response.success?
+
+        response.parsed_response['items'].map do |item|
+        {
+            video_id: item.dig('id', 'videoId'),
+            title: item.dig('snippet', 'title'),
+            thumbnail_url: item.dig('snippet', 'thumbnails', 'high', 'url'),
+            published_at: item.dig('snippet', 'publishedAt'),
+            channel_title: item.dig('snippet', 'channelTitle')
+        }
+        end
+    end
+
+    private
+
+    def log_response(response)
+        return unless Rails.env.development?
+
+        puts "[YouTube API] #{response.code} - #{response.parsed_response.inspect}"
+    end
+    end
 end
